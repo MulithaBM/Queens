c09764b5816eb975ff2df53a08f2df68ac3638b8
MulithaBM <mulithabanukamoremada123@gmail.com>
2024-08-17 08:52:19 +0530

    Unit tests for grid initialization

--- Queens.Tests/GridTests.cs
+++ Queens.Tests/GridTests.cs
@@ -5,7 +5,17 @@
         [Fact]
         public void Constructor_ProperInitialization()
         {
-            
+            List<List<int>> inputGrid =
+            [
+                [1, 2],
+                [3, 4]
+            ];
+
+            Grid grid = new(inputGrid);
+
+            Assert.Equal(2, grid.Rows);
+            Assert.Equal(2, grid.Columns);
+            Assert.Equal(4, grid.Groups.Count);
         }
     }
 }
